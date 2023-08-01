900d520d334a0682fe5c8deaadc6a4e8d9e2bebc
Andrew LIndzon <andrew@ashlin.ca>
2023-08-01 15:22:36 -0400

    Update definitions.d.ts

--- definitions.d.ts
+++ definitions.d.ts
@@ -4,6 +4,8 @@ export interface WatcherOptions {
     requestPermissions?: boolean;
     stale?: boolean;
     distanceFilter?: number;
+     interval?: number;
+    maxWaitTime?: number;
     numUpdates?:number;
 }
 
